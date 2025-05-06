e66ab52d607b84617869ae60b7de5e1b0f2dcdc8
frank1411 <fsanta01@gmail.com>
2025-05-06 11:58:23 -0400

    Configurar variables de entorno y optimización de Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,47 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import ViteImageOptimizer from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteImageOptimizer({
+    compress: {
+      jpeg: {
+        quality: 75,
+      },
+      png: {
+        quality: 75,
+      },
+    },
+    webp: {
+      quality: 75,
+    },
+  })],
   optimizeDeps: {
     exclude: ['lucide-react'],
+    include: ['react', 'react-dom'],
   },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
+  performance: {
+    hints: 'warning',
+    maxAssetSize: 500 * 1024,
+    maxEntrypointSize: 500 * 1024
+  }
 });
