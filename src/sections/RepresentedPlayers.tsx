9d4354d17832fc350208e9e57db3eb9edb8d05de
frank1411 <fsanta01@gmail.com>
2025-09-25 19:50:39 -0400

    Corregir importación de Button y eliminar archivo duplicado

--- src/sections/RepresentedPlayers.tsx
+++ src/sections/RepresentedPlayers.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { PlayerCard } from '../components/PlayerCard';
-import { Button } from '../components/ui/Button';
+import Button from '../components/Button';
 import { ArrowRight } from 'lucide-react';
 
 // Definir interfaz de Player
